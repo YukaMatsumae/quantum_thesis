f27ff16c255bfdc36dfa48a6eef9999671bdab36
yukamatsu <matuyuka.ray@gmail.com>
2026-04-19 18:33:43 +0900

    add square wave

--- Digital_Modulations_using_Pyhon/signalgen.py
+++ Digital_Modulations_using_Pyhon/signalgen.py
@@ -1,6 +1,7 @@
 #正弦波の生成(sine_wave_demoに関する)
 def sine_wave(f, overSampRate, phase, nCy1):
     import numpy as np
+
     fs = overSampRate * f #1秒間に何回サンプリングするか。
     t  = np.arange(0, nCy1 * 1/f - 1/fs, 1/fs )
     #1/fsは、サンプリングの間隔
@@ -22,6 +23,31 @@ def sine_wave_demo():
     nCy1 = 5 #周期の数
     (t, g) = sine_wave(f, overSampRate, phase, nCy1)
 
+    plt.plot(t, g)
+    plt.title("Sine wave f = "+ str(f) + "Hz")
+    plt.xlabel("Time[s]")
+    plt.ylabel("Amplitude")
+    plt.show()
+
+#方形波の生成
+def square_wave(f, overSampRate, nCy1):
+    import numpy as np
+
+    fs = overSampRate * f
+    t  = np.arange(0, nCy1 * 1/f - 1/fs, 1/fs)
+    g  = np.sign(np.sin(2 * np.pi * f * t))
+
+    return (t, g)
+
+#方形波の例、可視化
+def square_wave_demo():
+    import matplotlib.pyplot as plt
+    f = 10
+    overSampRate = 100
+    nCy1 = 5
+
+    (t, g) = square_wave(f, overSampRate, nCy1)
+    plt.figure(num = "square_wave_standard" )
     plt.plot(t, g)
     plt.title("Sine wave f = "+ str(f) + "Hz")
     plt.xlabel("Time[s]")
